27353c112f7e08638bdd4fb5f36a630d24fdaf30
HeonSeok98 <109660331+HeonSeok98@users.noreply.github.com>
2023-03-10 20:30:55 +0900

    feat: 필드추가

--- server/GoalForYou/src/main/java/com/example/GoalForYou/domain/Badge.java
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/domain/Badge.java
@@ -1,4 +1,17 @@
 package com.example.GoalForYou.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Badge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer badgeId;
+    private String badgeName;
+    private String badgeImage;
+
 }
--- server/GoalForYou/src/main/java/com/example/GoalForYou/domain/Feedback.java
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/domain/Feedback.java
@@ -1,4 +1,17 @@
 package com.example.GoalForYou.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer feedbackId;
+    private String title;
+    private String content;
+
 }
