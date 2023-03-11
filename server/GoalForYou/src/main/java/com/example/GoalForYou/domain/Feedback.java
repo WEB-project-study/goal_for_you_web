942652a7e3ec88d9537e7f7f985492767311925f
HeonSeok98 <109660331+HeonSeok98@users.noreply.github.com>
2023-03-12 01:41:46 +0900

    [Add] 피드백 타임스탬프 추가
    Feat: "피드백 타임스탬프 추가"
    필드추가

--- server/GoalForYou/src/main/java/com/example/GoalForYou/domain/Feedback.java
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/domain/Feedback.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.sql.Timestamp;
 
 @Entity
 public class Feedback {
@@ -15,5 +16,9 @@ public class Feedback {
     private String title;
 
     private String content;
+
+    private Timestamp writeDate;
+
+    private Timestamp editDate;
 
 }
