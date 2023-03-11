414db1ed639b96ff76cb4df703fbb7dc5afdc9c1
HeonSeok98 <109660331+HeonSeok98@users.noreply.github.com>
2023-03-12 02:30:48 +0900

    [Add] 리포지토리 추가
    리포지토리 추가
    
    jpa 설정추가

--- server/GoalForYou/src/main/java/com/example/GoalForYou/repository/BadgeRepository.java
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/repository/BadgeRepository.java
@@ -1,7 +1,9 @@
 package com.example.GoalForYou.repository;
 
+import com.example.GoalForYou.domain.Badge;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BadgeRepository {
+public interface BadgeRepository extends JpaRepository<Badge, Long> {
 }
--- server/GoalForYou/src/main/java/com/example/GoalForYou/repository/FeedbackRepository.java
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/repository/FeedbackRepository.java
@@ -1,7 +1,9 @@
 package com.example.GoalForYou.repository;
 
+import com.example.GoalForYou.domain.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FeedbackRepository {
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 }
