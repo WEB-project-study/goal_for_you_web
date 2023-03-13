d0b7217b454bce6c9fa7b22147fcc8a337049c83
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-03-13 19:25:11 +0900

    [Add] 유저 도메인 구현
    feat: @Entity 를 통한 유저 도메인 구현
    
    DB에 상응하는 @Column을 확인하고 일치하면 데이터를 객체로 저장 및 불러오기할 수 있습니다.

---
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/domain/User.java
@@ -0,0 +1,77 @@
+package com.example.GoalForYou.domain;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Locale;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    /** Primary **/
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "UID") @NotNull
+    private long uid;
+
+    @Column(name = "user_email") @NotNull
+    private String email;
+
+    @Column(name = "user_name") @NotNull
+    private String name;
+
+    /** 가입일 **/
+    @Column(name = "user_reg_date") @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date regDate;
+
+    @PrePersist
+    public void prePersist() {
+        this.regDate = Timestamp.valueOf(LocalDateTime.now());
+    }
+
+    @Column(name = "user_exp_date")
+    @Temporal(TemporalType.DATE)
+    private Date expDate;
+
+    public long getUid() {
+        return uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(Date regDate) {
+        this.regDate = regDate;
+    }
+
+    public Date getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(Date expDate) {
+        this.expDate = expDate;
+    }
+
+}
