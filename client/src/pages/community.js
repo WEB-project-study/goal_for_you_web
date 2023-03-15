f56034b5ccc79853ee3c52ae5ef4d4680fcb44b3
parksungmin1 <parksungmin1>
2023-03-15 23:29:08 +0900

    [feat] 커뮤니티 수정

--- client/src/pages/community.js
+++ client/src/pages/community.js
@@ -1,3 +1,21 @@
-export default function Community() {
-    return <>커뮤니티 페이지입니다.</>;
+import axios from 'axios';
+import React from 'react';
+import { useState } from 'react';
+export default function Community(){
+    const [products , setProducts] = useState([]);
+    return(
+        <div>
+            <h1>전체 게시물</h1>
+            <button onClick={()=>{
+                 axios.get('https://codingapple1.github.io/shop/data2.json')
+                      .then((result)=>{ setProducts([...products, ...result.data]) })
+                      .catch(()=>{
+                            console.log('fail');
+                            })
+                    }}>더보기</button>
+            <li>
+                {products.map((p) => <li>{p.title}</li>)}
+            </li>
+        </div>
+    );
 }
