02c769c94c052c5a1f72c04244e7b3f9da4ff0f4
nakyoung98 <66313756+nakyoung98@users.noreply.github.com>
2023-03-16 23:37:51 +0900

    [ADD] 유저 예외 처리핸들러 구현
    feat: throw되는 특정 예외 class들에 대하여 Http응답으로 전달될 ResponseEntity 객체 생성 기능
    
    1. 이미 존재하는 유저 409
    2. 이미 존재하는 닉네임 409
    3. 이미 탈퇴한 유저 409

---
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/exception/user/UserExceptionHandler.java
@@ -0,0 +1,81 @@
+package com.example.GoalForYou.exception.user;
+
+import com.example.GoalForYou.exception.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ * [UserExceptionHandler]
+ * User Repository를 관리하며 발생하는 예외를 처리
+ *
+ * @author 낙경
+ * @version 1.0.0
+ * @date 2023-03-13
+ ***/
+@ControllerAdvice
+public class UserExceptionHandler {
+
+    /**
+     *
+     * [userAlreadyExist]
+     * 이미 유저가 존재할 경우 충돌발생알림
+     * 전달될 Http응답
+     *
+     * @param exception
+     * @return HTTP 409 CONFLICT
+     * @author 낙경
+     * @version 1.0.0
+     * @date 2023-03-16
+     *
+    **/
+    @ExceptionHandler(UserAlreadyExistException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ErrorResponse> userAlreadyExist(UserAlreadyExistException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.toString(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    /**
+     *
+     * [userNameExist]
+     * 이미 동일한 닉네임의 유저가 존재할 경우 충돌발생알림
+     * 전달될 Http응답
+     * @param exception
+     * @return HTTP 409 CONFLICT
+     * @author 낙경
+     * @version 1.0.0
+     * @date 2023-03-16
+     *
+    **/
+    @ExceptionHandler(UserNameConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ErrorResponse> userNameExist(UserNameConflictException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.toString(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    /**
+     *
+     * [userAlreadyExpired]
+     * 이미 유저가 탈퇴한 회원으로 확인될 경우
+     * 중복요청 충돌 발생알림
+     * 전달될 Http응답
+     * @param exception
+     * @return HTTP 409 CONFLICT
+     * @author 낙경
+     * @version 1.0.0
+     * @date 2023-03-16
+     *
+    **/
+    @ExceptionHandler(UserAlreadyExpiredException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ErrorResponse> userAlreadyExpired(UserAlreadyExpiredException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.toString(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+}
+
