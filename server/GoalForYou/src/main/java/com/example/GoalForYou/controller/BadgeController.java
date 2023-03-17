38dfe156e26d274013702d4136bb686a0f6b615c
HeonSeok98 <109660331+HeonSeok98@users.noreply.github.com>
2023-03-18 00:27:59 +0900

    [Add] controller 추가
    controller 추가
    
    컨트롤러 작업

--- server/GoalForYou/src/main/java/com/example/GoalForYou/controller/BadgeController.java
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/controller/BadgeController.java
@@ -1,7 +1,12 @@
 package com.example.GoalForYou.controller;
 
+import com.example.GoalForYou.service.BadgeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BadgeController {
+
+    @Autowired
+    private BadgeService badgeService;
 }
--- server/GoalForYou/src/main/java/com/example/GoalForYou/controller/FeedbackController.java
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/controller/FeedbackController.java
@@ -1,7 +1,41 @@
 package com.example.GoalForYou.controller;
 
+import com.example.GoalForYou.service.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FeedbackController {
+
+    @Autowired
+    private FeedbackService feedbackService;
+
+    @PostMapping()
+    public String feedbackCreate() {
+
+        return null;
+    }
+
+    @GetMapping()
+    public String feedbackRead() {
+
+        return null;
+    }
+
+    @PostMapping()
+    public String feedbackUpdate() {
+
+        return null;
+    }
+
+    @GetMapping()
+    public String feedbackDelete(Long id) {
+
+        feedbackService.deleteFeedback(id);
+
+        return null;
+    }
 }
+
