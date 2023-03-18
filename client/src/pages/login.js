1d8c3d988b2766910354d9a4de97f1b2f5c16e50
IT_duncan <105710796+duncan1409@users.noreply.github.com>
2023-03-18 15:25:24 +0900

    [Add] google OAuth
    Feat: 로그인 구현 중
    
    Google 로그인 구현 중입니다.
    
    Fixes: 토큰 인식을 못하는 거 같은데 수정 중

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,24 +1,46 @@
-import Link from "next/link";
+import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+// import Link from "next/link";
 
-export default function Login() {
-    return (
-        
-        <nav className="flex flex-wrap items-center justify-center p-2 text-base font_local md:ml-auto">
-            <div className="p-1">
-                <Link legacyBehavior href="/ongoing">
-                    <a className="inline-flex items-center px-3 py-1 mt-4 text-white bg-blue-500 border-0 rounded-xl focus:outline-none hover:bg-sky-300 hover:text-black hover: md:mt-0">
-                        Google 계정으로 로그인 하기
-                    </a>
-                </Link>
-            </div>
-            <div className="p-1">
-                <Link legacyBehavior href="/mypage">
-                    <a className="inline-flex items-center px-3 py-1 mt-4 text-white bg-yellow-400 border-0 rounded-xl focus:outline-none hover:bg-yellow-200 hover:text-black hover: md:mt-0">
-                        카카오 계정으로 로그인 하기
-                    </a>
-                </Link>
-            </div>
-        </nav>
-        
-    );
-}
+const login = () => {
+    const { data: session } = useSession();
+
+    if (session) {
+        return (
+            <>
+                Welcome, {session.user.email}
+                <button onClick={() => signOut()}>signOut</button>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <p>You are not signed in.</p>
+                <button onClick={() => signIn()}>signIn</button>
+            </>
+        );
+    }
+};
+
+// export default function Login() {
+//     return (
+//         <nav className="flex flex-wrap items-center justify-center p-2 text-base font_local md:ml-auto">
+//             <div className="p-1">
+//                 <Link legacyBehavior href="/ongoing">
+//                     <a className="inline-flex items-center px-3 py-1 mt-4 text-white bg-blue-500 border-0 rounded-xl focus:outline-none hover:bg-sky-300 hover:text-black hover: md:mt-0">
+//                         Google 계정으로 로그인 하기
+//                     </a>
+//                 </Link>
+//             </div>
+//             <div className="p-1">
+//                 <Link legacyBehavior href="/mypage">
+//                     <a className="inline-flex items-center px-3 py-1 mt-4 text-white bg-yellow-400 border-0 rounded-xl focus:outline-none hover:bg-yellow-200 hover:text-black hover: md:mt-0">
+//                         카카오 계정으로 로그인 하기
+//                     </a>
+//                 </Link>
+//             </div>
+//         </nav>
+//     );
+// }
+
+export default login;
