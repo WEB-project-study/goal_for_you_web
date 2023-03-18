e481419003c7b3a6a73b4ec6f89f2d26871b8f80
HeonSeok98 <109660331+HeonSeok98@users.noreply.github.com>
2023-03-18 20:27:22 +0900

    [Add] update기능 추가
    Feat: update 기능 추가
    
    그러하다

--- server/GoalForYou/src/main/java/com/example/GoalForYou/service/BadgeService.java
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/service/BadgeService.java
@@ -1,6 +1,7 @@
 package com.example.GoalForYou.service;
 
 import com.example.GoalForYou.domain.Badge;
+import com.example.GoalForYou.domain.User;
 import com.example.GoalForYou.repository.BadgeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,13 @@ public class BadgeService {
     @Autowired
     private BadgeRepository badgeRepository;
 
-    public Long createBadge(Badge badge) {
+    public Long createBadge(User user, String badgeName, String badgeImage) {
+
+        Badge badge = new Badge();
+
+        badge.setUser(user);
+        badge.setBadgeName(badgeName);
+        badge.setBadgeImage(badgeImage);
 
         badgeRepository.save(badge);
         return badge.getBadgeId();
@@ -27,6 +34,14 @@ public class BadgeService {
     public List<Badge> getBadgeList() {
 
         return badgeRepository.findAll();
+    }
+
+    public Long updateBadge(Badge badge, String badgeName, String badgeImage) {
+
+        badge.setBadgeName(badgeName);
+        badge.setBadgeImage(badgeImage);
+
+        return badge.getBadgeId();
     }
 
     public void deleteBadge(Long id) {
--- server/GoalForYou/src/main/java/com/example/GoalForYou/service/FeedbackService.java
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/service/FeedbackService.java
@@ -1,10 +1,12 @@
 package com.example.GoalForYou.service;
 
 import com.example.GoalForYou.domain.Feedback;
+import com.example.GoalForYou.domain.User;
 import com.example.GoalForYou.repository.FeedbackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
@@ -13,7 +15,15 @@ public class FeedbackService {
     @Autowired
     private FeedbackRepository feedbackRepository;
 
-    public Long createFeedback(Feedback feedback) {
+    public Long createFeedback(User user, String title, String content) {
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        Feedback feedback = new Feedback();
+
+        feedback.setUser(user);
+        feedback.setTitle(title);
+        feedback.setContent(content);
+        feedback.setWriteDate(now);
+        feedback.setEditDate(now);
 
         feedbackRepository.save(feedback);
         return feedback.getFeedbackId();
@@ -25,6 +35,14 @@ public class FeedbackService {
 
     public List<Feedback> getFeedbackList() {
         return feedbackRepository.findAll();
+    }
+
+    public Long updateFeedback(Feedback feedback,String title, String content) {
+
+        feedback.setTitle(title);
+        feedback.setContent(content);
+
+        return feedback.getFeedbackId();
     }
 
     public void deleteFeedback(Long id) {
