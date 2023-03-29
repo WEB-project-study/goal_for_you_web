f3282b6fe1998275b93c8f74bf560aba6ec53268
yeoni98 <dayeon98@ajou.ac.kr>
2023-03-29 19:24:54 +0900

    CommunityController 수정
    .

--- server/GoalForYou/src/main/java/com/example/GoalForYou/controller/CommunityController.java
+++ server/GoalForYou/src/main/java/com/example/GoalForYou/controller/CommunityController.java
@@ -1,24 +1,26 @@
 package com.example.GoalForYou.controller;
 
 
+import com.example.GoalForYou.domain.File;
 import com.example.GoalForYou.dto.CommunityDto;
 import com.example.GoalForYou.dto.FileDto;
 import com.example.GoalForYou.service.CommunityService;
 import com.example.GoalForYou.service.FileService;
 import com.example.GoalForYou.util.MD5Generator;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Path;
 import java.util.List;
 @Controller
 //@AllArgsConstructor
 public class CommunityController {
     @Autowired
     private CommunityService communityService;
+    @Autowired
     private FileService fileService;
 
     /** 게시글 작성 */
@@ -27,7 +29,36 @@ public class CommunityController {
 
     @PostMapping("/community/create/{id}")
     @ResponseBody
-    public String communityCreate(CommunityDto communityDto){
+    public String communityCreate(@RequestParam("file") MultipartFile files, CommunityDto communityDto){
+
+            try {
+                String origFilename = files.getOriginalFilename();
+                String filename = new MD5Generator(origFilename).toString();
+                /* 실행되는 위치의 'files' 폴더에 파일이 저장됩니다. */
+                String savePath = System.getProperty("user.dir") + "\\files";
+                /* 파일이 저장되는 폴더가 없으면 폴더를 생성합니다. */
+                if (!new File(savePath).exists()) {
+                    try{
+                        new File(savePath).mkdir();
+                    }
+                    catch(Exception e){
+                        e.getStackTrace();
+                    }
+                }
+                String filePath = savePath + "\\" + filename;
+                files.transferTo((Path) new File(filePath));
+
+                FileDto fileDto = new FileDto();
+                fileDto.setOrigFilename(origFilename);
+                fileDto.setFilename(filename);
+                fileDto.setFilePath(filePath);
+
+                Long fileId = fileService.saveFile(fileDto);
+                fileDto.setFileId(fileId); //communityDto.setFileId(fileId); 는 왜 안 될까?
+                communityService.saveCommunity(communityDto);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         communityService.saveCommunity(communityDto);
         return "redirect:/";
     }
@@ -62,9 +93,9 @@ public class CommunityController {
 //        return "community/list.html";
 //        //list => 게시글 리스트 페이지
 //    }
-    /** 게시글 상세 조회 페이지 (클릭 했을 때 상세 조회)*/
+    /** 게시글 상세 조회 페이지 (클릭했을 때 상세 조회)*/
     @RequestMapping(value = "/post/{id}" , method = RequestMethod.GET)
-    // @GetMapping("/post/{id}")
+//    @GetMapping("/community/{id}")
     public String detail(@PathVariable("id") Long id, Model model) {
         CommunityDto communityDTO = (CommunityDto) communityService.getCommunity(id);
         model.addAttribute("communityDto", communityDTO);
@@ -80,7 +111,7 @@ public class CommunityController {
 //        return "redirect:/";
 //    }
     /** 게시글 수정 */
-    @PutMapping("/community/update/{id}")
+    @RequestMapping(value = "/community/update/{id}" , method = {RequestMethod.POST, RequestMethod.PUT})
     public String update(CommunityDto communityDTO) {
         communityService.saveCommunity(communityDTO);
 
@@ -88,17 +119,17 @@ public class CommunityController {
     }
 
     /** 게시글 삭제 */
-    @DeleteMapping("/community/{id}")
+    @DeleteMapping("/community/delete/{id}")
     public String delete(@PathVariable("id") Long id) {
         communityService.deleteCommunity(id);
 
         return "redirect:/";
     }
     /**이미지 파일*/
-    public CommunityController(CommunityService communityService, FileService fileService) {
-        this.communityService = communityService;
-        this.fileService = fileService;
-    }
+//    public CommunityController(CommunityService communityService, FileService fileService) {
+//        this.communityService = communityService;
+//        this.fileService = fileService;
+//    }
 //    @PostMapping("/post")
 //    public String write(@RequestParam("file") MultipartFile files, CommunityDto communityDto) {
 //        try {
